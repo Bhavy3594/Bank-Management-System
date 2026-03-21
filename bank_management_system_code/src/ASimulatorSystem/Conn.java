4ac5ca0a3a161aebdf2dac98cab1c0482929ad1a
Bhavy <bhavy3594@gmail.com>
2026-03-21 12:11:33 +0530

    first commit

---
+++ bank_management_system_code/src/ASimulatorSystem/Conn.java
@@ -0,0 +1,19 @@
+package ASimulatorSystem;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","[REDACTED_DB_PASSWORD]");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
